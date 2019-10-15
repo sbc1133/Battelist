733c78e6b1b56d43397294362e351e4f76f685bc
Shivalibhalla <shivalibhalla@gmail.com>
2019-10-15 15:01:06 -0700

    adding stuff

--- models/task.js
+++ models/task.js
@@ -1,36 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
-  var Task = sequelize.define("Task", {
-      taskName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      assigneeName: {
+    var Task = sequelize.define("Task", {
+        taskName: {
           type: DataTypes.STRING,
           allowNull: false,
           validate: {
             len: [1]
           }
         },
-<<<<<<< HEAD
-        selfAssigned: {
-          type: DataTypes.BOOLEAN,
-
-        },
-        score: {
-          type: DataTypes.INTEGER,
-        },
-      
-    });
-
-    Task.associate = function(models) {
-      // Associating Author with Posts
-      // When an Author is deleted, also delete any associated Posts
-      Task.belongsTo(models.User, {
-        onDelete: "cascade"
-=======
         assigneeName: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -42,28 +18,30 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.BOOLEAN,
  
           },
-<<<<<<< HEAD
-=======
         
 
->>>>>>> 1eddcf3379ce89873fbbccf77e2f640af9483249
           score: {
             type: DataTypes.INTEGER,
           },
         
->>>>>>> 85caa2aabb9efcbbb245d4c8805504e84904c0e5
       });
 
-      Task.belongsTo(models.Assignee,{
-        onDelete: "cascade"
-      })
+      Task.associate = function(models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        Task.belongsTo(models.User, {
+          onDelete: "cascade"
+        });
+
+        Task.belongsTo(models.Assignee,{
+          onDelete: "cascade"
+        })
+      };
+
+    
+      return Task;
+
+
     };
-
-  
-    return Task;
-
-
-  };
-
 
 
