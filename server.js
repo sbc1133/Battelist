df23d2917aea7bfae3d9bfec97653b2a31fd74e0
Michelle Santiago <msantiago2222@yahoo.com>
2019-10-16 15:57:48 -0700

    resetting db

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ require("./controllers/user_api_routes.js")(app);
 //  SYNC SEQUELIZE MODELS AND START EXPRESS APP
 // =============================================================================
 
-db.sequelize.sync({ force: false}).then(function() {
+db.sequelize.sync({ force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
